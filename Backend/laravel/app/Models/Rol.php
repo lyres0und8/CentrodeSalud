227aeee1f1a0f7f62e0512d4b770c05fdb13743b
Alex Pérez <129556633+MoxhaSB@users.noreply.github.com>
2025-04-29 23:48:45 -0400

    feat : se arreglan bugs
    error en nombre de la clase de Usuario.php.

--- Backend/laravel/app/Models/Rol.php
+++ Backend/laravel/app/Models/Rol.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Usuario;
 
 class Rol extends Model
 {
--- Backend/laravel/app/Models/Usuario.php
+++ Backend/laravel/app/Models/Usuario.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject
+class Usuario extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -29,7 +29,7 @@ class User extends Authenticatable implements JWTSubject
         'habilitado',
     ];
 
-    
+
     /**
      * The attributes that should be hidden for serialization.
      *
