1654cdc41d152758794aa29acaa0c336781b1003
Alex Pérez <129556633+MoxhaSB@users.noreply.github.com>
2025-04-29 22:01:16 -0400

    feat : se añaden las dependencias de php y node.js

---
+++ Backend/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'moxha-sb/backend',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b3dc7af4e25f90e28baa579e05e9e479e0c152d8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'moxha-sb/backend' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b3dc7af4e25f90e28baa579e05e9e479e0c152d8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
